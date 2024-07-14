b52d8cdc5a66cad5d9162b1c70910ef8da9c866c
tracer-ics2023 <tracer@njuics.org>
2024-07-14 18:26:44 +0800

    >  compile NEMU 221220000 张三 Linux asus 6.1.92-1-MANJARO #1 SMP PREEMPT_DYNAMIC Mon May 27 03:37:00 UTC 2024 x86_64 GNU/Linux  18:26:44 up  5:35,  1 user,  load average: 0.56, 0.67, 0.56

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -84,6 +84,7 @@ void scan_wp(){
     strncpy(expr_buffer, p->expr, 256);
     word_t val = expr(expr_buffer,&success);
     if(val != p->val){//值发生了变化
+    printf("Old value %d; New value %d\n",p->val,val);
       flag = true;
       p->val = val;
     }
