d1dbb0e481ff6d664a29b34a9f6dad19f23033c5
tracer-ics2023 <tracer@njuics.org>
2024-08-14 19:43:34 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  19:43:34 up  3:42,  2 users,  load average: 0.94, 1.04, 0.94

--- nemu/include/memory/host.h
+++ nemu/include/memory/host.h
@@ -35,7 +35,7 @@ static inline void host_write(void *addr, int len, word_t data) {
     case 2: *(uint16_t *)addr = data; return;
     case 4: *(uint32_t *)addr = data; return;
     case 8: *(uint64_t *)addr = data; return;
-    IFDEF(CONFIG_ISA64, case 8: *(uint64_t *)addr = data;return);
+    // IFDEF(CONFIG_ISA64, case 8: *(uint64_t *)addr = data;return);
     IFDEF(CONFIG_RT_CHECK, default: assert(0));
   }
 }
