e7d373a498b7064e868d42556f57c79d8bded294
tracer-ics2023 <tracer@njuics.org>
2024-08-15 16:21:22 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  16:21:22 up  2:57,  2 users,  load average: 3.50, 1.48, 1.17

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -100,7 +100,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
 static int decode_exec(Decode *s) {
   int rd = 0;
   word_t src1 = 0, src2 = 0, imm = 0;
-  s->dnpc = s->snpc;
+  s->dnpc = s->pc;
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */)                   \
