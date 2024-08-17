019e34f59034530cd5ac4bce35f98625c1283d81
tracer-ics2023 <tracer@njuics.org>
2024-08-17 15:20:19 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  15:20:19 up  5:46,  2 users,  load average: 0.87, 0.76, 0.79

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -140,7 +140,7 @@ static int decode_exec(Decode *s) {
           R(rd) = src1 >> shamt);
 
   INSTPAT("000000? ????? ????? 001 ????? 0010011", slli, I,
-          int32_t shamt = BITS(imm,25,20);
+          int32_t shamt = BITS(s->isa.inst.val,25,20);
           printf("slli: src1 = 0x%x, imm = 0x%x, shamt 0x%x\n", src1, imm,shamt);
           R(rd) = src1 << imm);
   // INSTPAT("0000000 ????? ????? 001 ????? 0110011", sll, R,
