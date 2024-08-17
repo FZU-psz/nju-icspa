e38888a22806ca30555d3d69931d2648344a9b00
tracer-ics2023 <tracer@njuics.org>
2024-08-17 15:14:53 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  15:14:53 up  5:41,  2 users,  load average: 0.73, 0.63, 0.78

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -136,13 +136,13 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("000000? ????? ????? 101 ????? 0010011", srli, I,
           uint32_t shamt = BITS(imm,4,0);
-          printf("s = 0x%x, imm = 0x%0x, shamt = 0x%x\n", s->isa.inst.val, imm,shamt);
-          R(rd) = src1 >> imm);
+          // printf("s = 0x%x, imm = 0x%0x, shamt = 0x%x\n", s->isa.inst.val, imm,shamt);
+          R(rd) = src1 >> shamt);
 
-  // INSTPAT("000000? ????? ????? 001 ????? 0010011", slli, I,
-  //         // uint32_t shamt = BITS(imm,4,0);
-  //         printf("src1 = %d, imm = %d\n", src1, imm);
-  //         R(rd) = src1 << imm);
+  INSTPAT("000000? ????? ????? 001 ????? 0010011", slli, I,
+          uint32_t shamt = BITS(imm,4,0);
+          printf("slli: src1 = 0x%x, imm = 0x%x, shamt 0x%x\n", src1, imm,shamt);
+          R(rd) = src1 << imm);
   // INSTPAT("0000000 ????? ????? 001 ????? 0110011", sll, R,
   //         uint32_t shamt = BITS(src2,4,0);
   //         R(rd) = src1 << shamt);
