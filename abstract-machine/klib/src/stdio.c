5a49e878e4356b33d344d62459edd2282063fabe
tracer-ics2023 <tracer@njuics.org>
2024-08-18 19:22:22 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  19:22:22 up  8:52,  2 users,  load average: 1.16, 0.96, 0.81

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,6 +1,7 @@
 #include <am.h>
-#include <klib.h>
+// #include <cstdarg>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
@@ -14,7 +15,50 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  // write formated str into out buffer
+  va_list args;
+  va_start(args, fmt); // get args from fmt str
+  int len = 0;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+      case 'd': {
+        int num = va_arg(args, int);
+        // convert int to str
+        while (num) {
+          *out = num % 10 + '0';
+          out++;
+          num /= 10;
+          len++;
+        }
+        break;
+      }
+      case 's': {
+        char *str = va_arg(args, char *);
+        while (*str) {
+          *out = *str;
+          out++;
+          str++;
+          len++;
+        }
+        break;
+      }
+      default:
+        break;
+      }
+    } else {
+      *out = *fmt;
+      out++;
+      len++;
+    }
+    fmt++;
+  }
+  *out = '\0';
+  len++;
+  va_end(args);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
