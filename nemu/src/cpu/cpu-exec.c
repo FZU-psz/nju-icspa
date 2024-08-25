3f7e537cf57a96aad578cc083ffa2f05b512bb94
tracer-ics2023 <tracer@njuics.org>
2024-08-25 12:07:00 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  12:07:00 up  2:30,  2 users,  load average: 2.87, 1.91, 1.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,11 +80,10 @@ static void execute(uint64_t n) {
   Decode s;
   
   for (;n > 0; n --) {
-    printf("n = %ld\n", n);
-    if(n>0) printf("yes\n");
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
+    printf("=========\n");
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
