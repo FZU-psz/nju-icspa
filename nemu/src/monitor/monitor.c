b7ba3814e02eb6d3a3ba4ce0afcb86e1f773a9f7
tracer-ics2023 <tracer@njuics.org>
2024-08-26 21:23:02 +0800

    >  compile NEMU 221220000 张三 Linux asus 5.15.164-1-MANJARO #1 SMP PREEMPT Sat Jul 27 13:08:43 UTC 2024 x86_64 GNU/Linux  21:23:02 up 11:43,  2 users,  load average: 0.42, 0.81, 0.89

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -111,7 +111,7 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Parse arguments. */
   parse_args(argc, argv);
-  parse_elf(elf_file);
+  // parse_elf(elf_file);
   /* Set random seed. */
   init_rand();
 
